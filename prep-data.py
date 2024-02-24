f73349443921575fa81cbeaa1e9b4ef900228193
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2024-02-24 01:26:30 +0000

    Revert changes

--- prep-data.py
+++ prep-data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import argparse
-import os
 
 # Read input data
 named_input = "data_path"
@@ -14,9 +13,7 @@ print(df)
 
 # Write output
 named_output = "processed_data"
-output_dir = "/workflow/outputs/{}".format(named_output)
-os.mkdir(output_dir) 
-df.to_csv("{}/data.csv".format(output_dir))
+df.to_csv("/workflow/outputs/{}".format(named_output))
 
 # Write output success
 with open("/workflow/outputs/_SUCCESS", "w+"):
